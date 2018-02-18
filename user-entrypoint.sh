34b97dbb69517d65b2c51cea8d0721408379ae0b
Yehuda Chikvashvili <yehudaac1@gmail.com>
2018-02-18 21:41:07 +0200

    Fix empty TIMEOUT case
    When TIMEOUT is not set or is empty, the container will try to use it
    The fix checking whether timeout is an integer and if it is, try to use it

--- user-entrypoint.sh
+++ user-entrypoint.sh
@@ -4,7 +4,9 @@ set -e
 
 PATH='/bin:/usr/bin:/usr/local/bin'
 CONTAINER_NAME=$(cat /dev/urandom | tr -dc 'a-f0-9' | fold -w64 | head -n 1)
-if [ -n $TIMEOUT ]; then
+
+# check whether TIMEOUT is an integer
+if [[ $TIMEOUT =~ ^[0-9]+$ ]]; then
   echo "You got $TIMEOUT seconds. GO!"
   (sleep $TIMEOUT && echo -e "${TIMEOUT_MESSAGE}\r" && docker rm -fv ${CONTAINER_NAME} > /dev/null) &
 fi
