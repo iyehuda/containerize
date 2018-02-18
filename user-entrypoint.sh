b62c7d149339b94d3c1a83ab4271c41df22213af
Yehuda Chikvashvili <yehudaac1@gmail.com>
2018-02-18 21:55:00 +0200

    Support images with dash /bin/sh
    Use grep instead of bash built-in regex syntax

--- user-entrypoint.sh
+++ user-entrypoint.sh
@@ -6,7 +6,7 @@ PATH='/bin:/usr/bin:/usr/local/bin'
 CONTAINER_NAME=$(cat /dev/urandom | tr -dc 'a-f0-9' | fold -w64 | head -n 1)
 
 # check whether TIMEOUT is an integer
-if [[ $TIMEOUT =~ ^[0-9]+$ ]]; then
+if echo $TIMEOUT | grep -Eq '^[+-]?[0-9]+$'; then
   echo "You got $TIMEOUT seconds. GO!"
   (sleep $TIMEOUT && echo -e "${TIMEOUT_MESSAGE}\r" && docker rm -fv ${CONTAINER_NAME} > /dev/null) &
 fi
